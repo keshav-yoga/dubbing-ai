ce7cfa8af38e0cfebad764cbd7e0f40a03383ef4
keshav-yoga <tredas1238u@gmail.com>
2025-04-23 21:27:24 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,5 @@
 # backend/app/models.py
-
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
