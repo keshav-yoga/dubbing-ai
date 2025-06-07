7d9f4888a33390e671cedcbf9fd234ab9e54e578
keshav-yoga <tredas1238u@gmail.com>
2025-06-07 23:34:44 +0530

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import routes from "./routes.jsx";
+import routes from "./routers.jsx";
 
 export default function App() {
   return <RouterProvider router={routes} />;
